b82f282a016caff76e3b71f2f6aeea1a0f470c41
hainguyenhn <mvamtv@gmail.com>
2014-11-20 04:44:20 -0800

    Completed check out system for customer.

--- src/Database.java
+++ src/Database.java
@@ -19,19 +19,19 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-public class Database 
+public class Database
 {
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
    static final String DB_URL = "jdbc:mysql://localhost:3306/";
 
    //  Database credentials
    static final String USER = "root";
    static final String PASS = "";
    static final String database = "warehouse";
-   
+
    static Connection conn = null;
    static Statement stmt = null;
-   
+
    //this method connect java application to database.
    public static void connection()
    {
@@ -43,17 +43,17 @@ public class Database
       System.out.println("Connecting to database...");
       conn = DriverManager.getConnection(DB_URL+database, USER, PASS);
 
-            
+
    }catch(SQLException se){
       //Handle errors for JDBC
       se.printStackTrace();
    }catch(Exception e){
       //Handle errors for Class.forName
       e.printStackTrace();
-   
+
    }//end try
    }//end connection
-   
+
    //this method handle user/admin log in.
    public static String login(String name, String pass)
    {
@@ -63,27 +63,28 @@ public class Database
         try {
             connection();
             String myStament = "SELECT role "
-                    + "FROM USERTABLE"  
+                    + "FROM USERTABLE"
                     + " WHERE username = " + username + " AND password = " + userpass;
             stmt = conn.createStatement();
             stmt.execute(myStament);
             ResultSet rs = stmt.getResultSet();
             rs.first();
             result = rs.getString("role");
-            }       
+            }
         catch (SQLException ex) {
-           
+
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-        }       
+        }
+        loadProg();
        return result;
    }
-   
+
    //this method add new customer to database.
    public static boolean addCustomer(ArrayList infor)
    {
         try {
             connection();
-          
+
             String myStament = "INSERT INTO usertable (userName,password,firstName,lastName,phone,address,role)"
                     + " Values ("
                     + infor.get(0).toString() +"," + infor.get(1).toString() +"," + infor.get(2).toString()+","
@@ -92,21 +93,21 @@ public class Database
             System.out.print(myStament);
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             if(stmt== null)
                  return false;
-             return true; 
+             return true;
         }
-        
-        catch (SQLException ex) {          
+
+        catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
        return false;
    }//and of addCustomer.
-   
+
    //this method add new item to ventory.
    public static boolean addItem(ArrayList item)
-   {   
+   {
         try {
             connection();
             String myStament = "INSERT INTO inventory (productname,description,categoryid,quantity,price)"
@@ -115,19 +116,19 @@ public class Database
                     + Integer.parseInt(item.get(3).toString()) +"," + Double.parseDouble(item.get(4).toString()) +")";
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             if(stmt== null)
                  return false;
-             return true; 
+             return true;
         }
-        
+
         catch (SQLException ex) {
-           
+
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
        return false;
    }
-   
+
    //this method search for customer.
    public static Customer searchCustomer(int myId,Customer myCustomer)
    {
@@ -136,10 +137,10 @@ public class Database
             String myStament = "SELECT customerid,userName,password,firstName,lastName,phone,address,role "
                     + "FROM usertable "
                     + "WHERE customerid = " + myId ;
-                 
+
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             ResultSet rs = stmt.getResultSet();
             if(!rs.first())
             {
@@ -159,18 +160,18 @@ public class Database
                 myCustomer.lastName = rs.getString("lastName");
                 myCustomer.phone = rs.getString("phone");
                 myCustomer.address = rs.getString("address");
-                myCustomer.role = rs.getString("role");              
+                myCustomer.role = rs.getString("role");
             }
             }
         }
-        
+
         catch (SQLException ex) {
-           
+
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return myCustomer; 
+        return myCustomer;
    }//end of searchCustomer method.
-   
+
    //Updates existing customer using myCustomer's information.
    public static boolean updateCustomer(int id,Customer myCustomer)
    {
@@ -178,101 +179,101 @@ public class Database
        try {
             connection();
             String myStament = "UPDATE usertable "
-                    + "SET userName = '"+myCustomer.userName+"', " + "password = '"+myCustomer.password+"', " 
+                    + "SET userName = '"+myCustomer.userName+"', " + "password = '"+myCustomer.password+"', "
                             +"firstName ='"+myCustomer.firstName+"', " + "lastName ='" + myCustomer.lastName+ "', "
                             + "phone ='" + myCustomer.phone+"', "+ "address = '"+myCustomer.address+"', "+ "role ='"+myCustomer.role+"'"
                     + "WHERE customerid = " + id ;
-                 
+
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             if(stmt.getUpdateCount() >0)
             {
                 success = true;
-            }        
+            }
         }
-        
+
         catch (SQLException ex) {
-           
+
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return success; 
+        return success;
    }
-   
+
    //this method delete item from inventory using item id.
    public static boolean deleteItem(int id)
    {
        boolean sucess = false;
          try {
             connection();
-            String myStament = "DELETE "+ 
+            String myStament = "DELETE "+
                         "FROM inventory "
-                    + "WHERE productid = " + id;      
+                    + "WHERE productid = " + id;
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             //check rows affected
             if(stmt.getUpdateCount() > 0)
             {
                 sucess =  true;
-            }           
-        }        
+            }
+        }
         catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return sucess; 
+        return sucess;
    } //end of deleteItem method.
-   
-   //this method update item's quantity in inventory
+
+   //handles quantity update by admin.
    public static boolean updateQuantity(int id, int quantity)
    {
        boolean sucess = false;
          try {
             connection();
-            String myStament = "UPDATE inventory "+ 
+            String myStament = "UPDATE inventory "+
                         " SET quantity = "+quantity
-                    + " WHERE productid = " + id;  
+                    + " WHERE productid = " + id;
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             //check rows affected
             if(stmt.getUpdateCount() > 0)
             {
                 sucess =  true;
-            }           
-        }        
+            }
+        }
         catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return sucess; 
+        return sucess;
    } //end of updateQuantity
-   
+
    //this method delete customer from customer table using id.
    public static boolean deleteCustomer(int id)
    {
        boolean sucess = false;
          try {
             connection();
-            String myStament = "DELETE "+ 
+            String myStament = "DELETE "+
                         " FROM usertable "
                     + " WHERE customerid = " + id
-                    + " AND role <> 'Admin'";      
+                    + " AND role <> 'Admin'";
             stmt = conn.createStatement();
             stmt.execute(myStament);
-            
+
             System.out.print(myStament);
             //check rows affected
             if(stmt.getUpdateCount() >0)
             {
                 sucess =  true;
-            }           
-        }        
+            }
+        }
         catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return sucess; 
-   }//end of deletecustomer method.   
-   
+        return sucess;
+   }//end of deletecustomer method.
+
    //populates data to table and return
    public static JTable displayTable(JTable myTable, String option)
    {
@@ -285,16 +286,16 @@ public class Database
         ResultSet rs = stmt.executeQuery(myStatement);
 
         //To remove previously added rows
-        while(myTable.getRowCount() > 0) 
+        while(myTable.getRowCount() > 0)
         {
             ((DefaultTableModel) myTable.getModel()).removeRow(0);
         }
         int numColumns = rs.getMetaData().getColumnCount();
         while(rs.next())
-        {  
+        {
             Object[] row = new Object[numColumns];
             for (int i = 1; i <= numColumns; i++)
-            {  
+            {
                 row[i - 1] = rs.getObject(i);
             }
             ((DefaultTableModel) myTable.getModel()).insertRow(rs.getRow()-1,row);
@@ -305,6 +306,90 @@ public class Database
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
    }
      return myTable;
+   }//end of displayTable
+
+   //handles customer check out, update inventory table.
+   public static boolean checkOut(String currentName,String itemName,int quantityBought)
+   {
+       boolean success = false;
+         try {
+            connection();
+            String myStament = "UPDATE inventory "+
+                        " SET quantity =  " +quantityBought
+                    + " WHERE inventory.productname = '" + itemName +"'"
+                    ;
+             System.out.println(myStament);
+            stmt = conn.createStatement();
+            stmt.execute(myStament);
+               if(!(stmt.getUpdateCount() > 0))
+            {
+                return success;
+            }
+               String set = "SET @customerName = '"+currentName +"'";
+               stmt.execute(set);
+               String out = "SET @mycustomerID = 0";
+               stmt.execute(out);
+               String call = "CALL getCustomerId(@customerName,@mycustomerID);";
+               stmt.execute(call);
+
+               //insert into transaction table.
+            String secondStatement = "INSERT INTO transaction (customerId) Values (@mycustomerID);";
+            stmt.execute(secondStatement);
+            System.out.println(secondStatement);
+
+             if(!(stmt.getUpdateCount() > 0))
+            {
+                return success;
+            }
+            //insert into sales record
+                 String setName = "SET @itemName = '"+itemName+"';";
+               String setoutId = "Set @itemId = 0;";
+               String setTransactionId = "SET @mytransactionID = (select MAX(transactionId) from transaction where transaction.customerId = @mycustomerID);";
+               String callGetIdMethod = "CALL getItemId(@itemName,@itemId);";
+               String getPrice = "Set @itemPrice = (SELECT price FROM inventory WHERE inventory.productid = @itemId);";
+            String thirdStatement = "INSERT INTO salerecord (transactionId,productId,quantity,price) Values(@mytransactionID,@itemId,"+quantityBought+",@itemPrice);";
+            stmt.execute(setName);
+            stmt.execute(setoutId);
+            stmt.execute(setTransactionId);
+            stmt.execute(callGetIdMethod);
+            stmt.execute(getPrice);
+            stmt.execute(thirdStatement);
+              if(!(stmt.getUpdateCount() > 0))
+            {
+                return success;
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+         //check if 3 tables has been updated.
+        success =true;
+        return success;
+   }//end of checkout
+
+   public static void loadProg()
+   {
+       String getcustomerIdMethod0 = "DROP PROCEDURE IF EXISTS getCustomerId;";
+       String getcustomerIdMethod = " CREATE PROCEDURE getCustomerId (IN myUserName VARCHAR(50),OUT customerIdOUT INT(4)) BEGIN  SELECT customerId INTO customerIdOUT  FROM usertable  WHERE usertable.username = myUserName;  END  ";
+       String getItemIdMethod0 = "DROP PROCEDURE IF EXISTS getItemId;";
+       String getItemIdMethod1 = "CREATE PROCEDURE getItemId(IN itemName VARCHAR(50), OUT itemID INT(10))\n" +
+"BEGIN\n" +
+"SELECT productid INTO itemID \n" +
+"FROM inventory \n" +
+"WHERE inventory.productname = itemName;\n" +
+"END";
+        try {
+            connection();
+            stmt = conn.createStatement();
+            stmt.execute(getcustomerIdMethod0);
+            stmt.execute(getcustomerIdMethod);
+            stmt.execute(getItemIdMethod0);
+            stmt.execute(getItemIdMethod1);
+        }
+         catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
    }
 }
 
